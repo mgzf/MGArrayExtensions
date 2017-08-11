01b04c4d15be431f21e1f3e1bf82a97bf3273f2a
Harly <Harly@SpiciedCrab.local>
2017-08-11 15:52:06 +0800

    f

--- MGArrayExtensions/Sources/ArrayExtension.swift
+++ MGArrayExtensions/Sources/ArrayExtension.swift
@@ -19,7 +19,7 @@ extension Collection
 
 extension Array {
     
-    func filteri(_ fn: (Element, Int) -> Bool) -> [Element] {
+    public func filteri(_ fn: (Element, Int) -> Bool) -> [Element] {
         var to = [Element]()
         var i = 0
         for x in self {
@@ -32,7 +32,7 @@ extension Array {
         return to
     }
     
-    func first(_ fn: (Element) -> Bool) -> Element? {
+    public func first(_ fn: (Element) -> Bool) -> Element? {
         for x in self {
             let t = x as Element
             if fn(t) {
@@ -42,7 +42,7 @@ extension Array {
         return nil
     }
     
-    func first(_ fn: (Element, Int) -> Bool) -> Element? {
+    public func first(_ fn: (Element, Int) -> Bool) -> Element? {
         var i = 0
         for x in self {
             let t = x as Element
@@ -54,15 +54,15 @@ extension Array {
         return nil
     }
     
-    func any(_ fn: (Element) -> Bool) -> Bool {
+    public func any(_ fn: (Element) -> Bool) -> Bool {
         return self.filter(fn).count > 0
     }
     
-    func all(_ fn: (Element) -> Bool) -> Bool {
+    public func all(_ fn: (Element) -> Bool) -> Bool {
         return self.filter(fn).count == self.count
     }
     
-    func expand<TResult>(_ fn: (Element) -> [TResult]?) -> [TResult] {
+    public func expand<TResult>(_ fn: (Element) -> [TResult]?) -> [TResult] {
         var to = [TResult]()
         for x in self {
             if let result = fn(x as Element) {
@@ -74,7 +74,7 @@ extension Array {
         return to
     }
     
-    func take(_ count:Int) -> [Element] {
+    public func take(_ count:Int) -> [Element] {
         var to = [Element]()
         var i = 0
         while i < self.count && i < count {
@@ -84,7 +84,7 @@ extension Array {
         return to
     }
     
-    func skip(_ count:Int) -> [Element] {
+    public func skip(_ count:Int) -> [Element] {
         var to = [Element]()
         var i = count
         while i < self.count {
@@ -94,7 +94,7 @@ extension Array {
         return to
     }
     
-    func takeWhile(_ fn: (Element) -> Bool) -> [Element] {
+    public func takeWhile(_ fn: (Element) -> Bool) -> [Element] {
         var to = [Element]()
         for x in self {
             let t = x as Element
@@ -108,7 +108,7 @@ extension Array {
         return to
     }
     
-    func skipWhile(_ fn: (Element) -> Bool) -> [Element] {
+    public func skipWhile(_ fn: (Element) -> Bool) -> [Element] {
         var to = [Element]()
         var keepSkipping = true
         for x in self {
@@ -123,7 +123,7 @@ extension Array {
         return to
     }
     
-    func firstWhere(_ fn: (Element) -> Bool) -> Element? {
+    public func firstWhere(_ fn: (Element) -> Bool) -> Element? {
         for x in self {
             if fn(x) {
                 return x
@@ -132,7 +132,7 @@ extension Array {
         return nil
     }
     
-    func firstWhere(_ fn: (Element) -> Bool, orElse: () -> Element) -> Element {
+    public func firstWhere(_ fn: (Element) -> Bool, orElse: () -> Element) -> Element {
         for x in self {
             if fn(x) {
                 return x
@@ -141,7 +141,7 @@ extension Array {
         return orElse()
     }
     
-    func sortBy(_ fns: ((Element,Element) -> Int)...) -> [Element]
+    public func sortBy(_ fns: ((Element,Element) -> Int)...) -> [Element]
     {
         return self.sorted { x, y in
             for f in fns {
@@ -154,15 +154,15 @@ extension Array {
         }
     }
     
-    func groupBy<Key : Hashable, Item>(_ fn:(Item) -> Key) -> [Group<Key,Item>] {
+    public func groupBy<Key : Hashable, Item>(_ fn:(Item) -> Key) -> [Group<Key,Item>] {
         return self.groupBy(fn, matchWith: nil, valueAs: nil)
     }
     
-    func groupBy<Key : Hashable, Item>(_ fn:(Item) -> Key, matchWith:((Key,Key) -> Bool)?) -> [Group<Key,Item>] {
+    public func groupBy<Key : Hashable, Item>(_ fn:(Item) -> Key, matchWith:((Key,Key) -> Bool)?) -> [Group<Key,Item>] {
         return self.groupBy(fn, matchWith: matchWith, valueAs: nil)
     }
     
-    func groupBy<Key : Hashable, Item>
+    public func groupBy<Key : Hashable, Item>
         (
         _ fn:        (Item) -> Key,
         matchWith: ((Key,Key) -> Bool)?,
@@ -198,7 +198,7 @@ extension Array {
         return map.values.map { $0 as Group<Key,Item> }
     }
     
-    func indexOf<T : Equatable>(_ x:T) -> Int? {
+    public func indexOf<T : Equatable>(_ x:T) -> Int? {
         for i in 0..<self.count {
             if self[i] as! T == x {
                 return i
@@ -207,7 +207,7 @@ extension Array {
         return nil
     }
     
-    func toDictionary<Key : Hashable, Item>(_ fn:(Item) -> Key) -> Dictionary<Key,Item> {
+    public func toDictionary<Key : Hashable, Item>(_ fn:(Item) -> Key) -> Dictionary<Key,Item> {
         var to = Dictionary<Key,Item>()
         for x in self {
             let e = x as! Item
@@ -218,47 +218,47 @@ extension Array {
     }
 
     
-    func sum<T : Addable>() -> T
+    public func sum<T : Addable>() -> T
     {
         return self.map { $0 as! T }.reduce(T()) { $0 + $1 }
     }
     
-    func sum<U, T : Addable>(_ fn: (U) -> T) -> T {
+    public func sum<U, T : Addable>(_ fn: (U) -> T) -> T {
         return self.map { fn($0 as! U) }.reduce(T()) { $0 + $1 }
     }
     
-    func minElement<U, T : Reducable>(_ fn: (U) -> T) -> T {
+    public func minElement<U, T : Reducable>(_ fn: (U) -> T) -> T {
         return self.map { fn($0 as! U) }.reduce(T.max()) { $0 < $1 ? $0 : $1 }
     }
     
-    func maxElement<U, T : Reducable>(_ fn: (U) -> T) -> T {
+    public func maxElement<U, T : Reducable>(_ fn: (U) -> T) -> T {
         return self.map { fn($0 as! U) }.reduce(T()) { $0 > $1 ? $0 : $1 }
     }
     
-    func avg<U, T : Averagable>(_ fn: (U) -> T) -> Double {
+    public func avg<U, T : Averagable>(_ fn: (U) -> T) -> Double {
         return self.map { fn($0 as! U) }.reduce(T()) { $0 + $1 } / self.count
     }
 }
 
 extension Array where Element : Averagable
 {
-    func avg() -> Double
+    public func avg() -> Double
     {
         return self.reduce(Element()) { $0 + $1 } / self.count
     }
 }
 
 
-protocol Addable {
+public protocol Addable {
     static func +(lhs: Self, rhs: Self) -> Self
     init()
 }
 
-protocol Reducable : Addable, Averagable, Comparable {
+public protocol Reducable : Addable, Averagable, Comparable {
     static func max() -> Self
 }
 
-protocol Averagable : Addable {
+public protocol Averagable : Addable {
     static func /(lhs: Self, rhs: Int) -> Double
 }
 
@@ -283,15 +283,15 @@ protocol Averagable : Addable {
 //}
 
 
-func distinct<T : Equatable>(_ this:[T]) -> [T] {
+public func distinct<T : Equatable>(_ this:[T]) -> [T] {
     return union(this)
 }
 
-func union<T : Equatable>(_ arrays:[T]...) -> [T] {
+public func union<T : Equatable>(_ arrays:[T]...) -> [T] {
     return _union(arrays)
 }
 
-func _union<T : Equatable>(_ arrays:[[T]]) -> [T] {
+public func _union<T : Equatable>(_ arrays:[[T]]) -> [T] {
     var to = [T]()
     for arr in arrays {
         outer: for x in arr {
@@ -307,7 +307,7 @@ func _union<T : Equatable>(_ arrays:[[T]]) -> [T] {
     return to
 }
 
-func intersection<T : Equatable>(_ arrays:[T]...) -> [T] {
+public func intersection<T : Equatable>(_ arrays:[T]...) -> [T] {
     let all: [T] = _union(arrays)
     var to = [T]()
     
@@ -330,7 +330,7 @@ func intersection<T : Equatable>(_ arrays:[T]...) -> [T] {
     return to
 }
 
-func difference<T : Equatable>(_ from:[T], other:[T]...) -> [T] {
+public func difference<T : Equatable>(_ from:[T], other:[T]...) -> [T] {
     var to = [T]()
     for arr in other {
         for x in from {
@@ -347,9 +347,9 @@ func difference<T : Equatable>(_ from:[T], other:[T]...) -> [T] {
 //   while let x = g.next() { .. }
 //
 //Generic classes not supported yet? Crashes XCode
-struct Group<Key,Item> : Sequence, CustomStringConvertible {
-    let key: Key
-    var items = [Item]()
+public struct Group<Key,Item> : Sequence, CustomStringConvertible {
+    public let key: Key
+    public var items = [Item]()
     
     init(key:Key) {
         self.key = key
@@ -359,11 +359,11 @@ struct Group<Key,Item> : Sequence, CustomStringConvertible {
         items.append(item)
     }
     
-    func makeIterator() -> IndexingIterator<[Item]> {
+    public func makeIterator() -> IndexingIterator<[Item]> {
         return items.makeIterator()
     }
     
-    var description: String {
+    public var description: String {
     var s = ""
         for x in items {
             if s.length > 0 {
@@ -375,7 +375,7 @@ struct Group<Key,Item> : Sequence, CustomStringConvertible {
     }
 }
 
-func join<T,U>(_ seq:[T], withSeq:[U], match:(T,U)->Bool) -> [(T,U)] {
+public func join<T,U>(_ seq:[T], withSeq:[U], match:(T,U)->Bool) -> [(T,U)] {
     return seq.expand { (x:T) in
         withSeq
             .filter { y in match(x,y) }
@@ -383,24 +383,24 @@ func join<T,U>(_ seq:[T], withSeq:[U], match:(T,U)->Bool) -> [(T,U)] {
     }
 }
 
-func joinGroup<T : Hashable,U>(_ seq:[T], withSeq:[U], match:(T,U)->Bool) -> [Group<T,(T,U)>] {
+public func joinGroup<T : Hashable,U>(_ seq:[T], withSeq:[U], match:(T,U)->Bool) -> [Group<T,(T,U)>] {
     return join(seq, withSeq: withSeq, match: match).groupBy { x -> T in
         let (t,_) = x
         return t
     }
 }
 
-func anagramComparer(_ a:String, b:String) -> Bool {
+public func anagramComparer(_ a:String, b:String) -> Bool {
     let aChars = Array(a.utf8CString)
     let bChars = Array(b.utf8CString)
     return aChars.sorted() == bChars.sorted()
 }
 
-func caseInsensitiveComparer (_ a:String,b:String) -> Bool {
+public func caseInsensitiveComparer (_ a:String,b:String) -> Bool {
     return a.uppercased().compare(b.uppercased()) == .orderedAscending
 }
 
-func compareIgnoreCase (_ a:String, _ b:String) -> Int {
+public func compareIgnoreCase (_ a:String, _ b:String) -> Int {
     switch a.uppercased().compare(b.uppercased()) {
     case .orderedAscending:
         return 1
@@ -411,7 +411,7 @@ func compareIgnoreCase (_ a:String, _ b:String) -> Int {
     }
 }
 
-func compare<T : Comparable>(_ a:T, _ b:T) -> Int {
+public func compare<T : Comparable>(_ a:T, _ b:T) -> Int {
     return a == b
         ? 0
         : a > b ? -1 : 1
